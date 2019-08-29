9fe0abd26c3d79399494afbb4da9069093baa6d4
Will Comfort <comfortwb21@gmail.com>
2019-08-29 16:10:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,7 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(4)
+  list_of_programming_languages.insert(3)
 end
 
   
